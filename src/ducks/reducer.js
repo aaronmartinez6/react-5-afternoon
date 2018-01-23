4144a3af89346a3a703882fec1be6b74055829b2
Aaron Martinez <aaronmartinez6@hotmail.com>
2018-01-22 21:01:57 -0700

    Finish through step 3

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,4 +1,4 @@
-function reducer(){
+function reducer() {
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
 
